82967fb86e06153d040e379bef74bf5f37ffb1f2
Martin Stenflo <stenflo@gmail.com>
2020-08-09 14:51:08 -0700

    add speech synthesis

--- script.js
+++ script.js
@@ -69,8 +69,18 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
+
   main.appendChild(box);
 };
+
+const message = new SpeechSynthesisUtterance();
 
 let voices = [];
 
@@ -85,6 +95,14 @@ function getVoices() {
 
     voicesSelect.appendChild(option);
   });
+}
+
+function setTextMessage(text) {
+  message.text = text;
+}
+
+function speakText() {
+  speechSynthesis.speak(message);
 }
 
 speechSynthesis.addEventListener('voiceschanged', getVoices)
